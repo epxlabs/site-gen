40a95f53b074370c6a9cb6d6334e913de2424f69
alexmartin1124 <alexseth1124@gmail.com>
2016-10-26 14:00:57 -0400

    fixes background

--- src/site_generator/core.clj
+++ src/site_generator/core.clj
@@ -89,7 +89,9 @@
    (assets/load-assets "public" ["/img/logos/epx_logo.svg"
                                  "/videos/11845277.mp4"
                                  "/img/landing.jpg"
-                                 "/img/epx-favicon.png"])))
+                                 "/img/epx-favicon.png"
+                                 "/blog_images/3/serverless-cost-savings-1.jpg"
+                                 "/blog_images/3/serverless-cost-savings-2.jpg"])))
 
 (def app
   ;; Optimus will serve our assets dynamically in development while still optimizing
