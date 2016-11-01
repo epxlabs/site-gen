a4c3db80040a8b87e57550ae7be3bfd0848e4d90
alexmartin1124 <alexseth1124@gmail.com>
2016-11-01 14:03:26 -0400

    tries to get fb share to work

--- src/site_generator/core.clj
+++ src/site_generator/core.clj
@@ -86,7 +86,8 @@
                "/js/views/view.home.js"
                "/js/views/view.contact.js"
                "/js/custom.js"
-               "/js/theme.init.js"]})
+               "/js/theme.init.js"
+               "/js/facebook.js"]})
    ;; Loads in images
    (assets/load-assets "public" ["/img/logos/epx_logo.svg"
                                  "/videos/11845277.mp4"
@@ -98,7 +99,7 @@
   (-> (s/serve-pages (get-pages))
       (optimus/wrap
        get-assets
-       op/all
+       op/none
        strategies/serve-live-assets)
       (ct/wrap-content-type)))
 
