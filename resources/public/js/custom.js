cd263ea4aa7794cb809d9c64a93754f87db7e136
alexmartin1124 <alexseth1124@gmail.com>
2016-11-02 15:39:29 -0400

    adds disqus

--- resources/public/js/custom.js
+++ resources/public/js/custom.js
@@ -45,3 +45,17 @@ $(document).ready(function(){
   }
   clickEvents()
   }, 1)})
+
+
+
+var disqus_config = function () {
+this.page.url = "http://www.epxlabs.com";  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = window.location.pathname; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = '//www-epxlabs-com.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
