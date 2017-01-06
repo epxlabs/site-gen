97483487c42dc4ee4d75924cb2cf479551246f06
alexmartin1124 <alexseth1124@gmail.com>
2017-01-06 11:39:27 -0500

    fixes navlinks bug

--- resources/public/js/custom.js
+++ resources/public/js/custom.js
@@ -1,8 +1,8 @@
 $('a[href="#contact"]').click(function(){
-   $('html, body').animate({
-        scrollTop: $(document).height()
-    }, 1500);
-    return false;
+  $('html, body').animate({
+    scrollTop: $(document).height()
+  }, 1500);
+  return false;
 })
 
 $(document).mousemove(function(event) {
@@ -21,46 +21,52 @@ var clickEvents = function(){
     else {
       var page = rawpage.slice(1).slice(0, -1)
     }
-  $('li#contact').removeClass('active')
-  $('li#' + page).addClass('dropdown active')
-})
+    $('li#contact').removeClass('active')
+    $('li#' + page).addClass('dropdown active')
+  })
 
-$('#footer').mouseleave(function(){
-  $('li#contact-us').addClass('active')
-})}
+  $('#footer').mouseleave(function(){
+    $('li#contact-us').addClass('active')
+  })}
 
-$(document).ready(function(){
-  /* This function changes the title to match the blog post on a blog post page*/
-  if($('.blog-post').length == 1){
-    var title = "EPX Labs, Inc. | " + $('h1').text()
-    $(document).prop('title', title)
-  }
-  clickEvents()
-  setInterval(function(){
-  if ($('li.active').length > 1){
-      if ($(currentMousePos).attr('id') == "contact"){
-        $($('li.active')[0]).removeClass('dropdown active')
+  $(document).ready(function(){
+    var links = $('.nav-links')
+    for (var i = 0; i < links.length; i++) {
+      var link = links[i]
+      var href = $(link).attr('href')
+      $(link).attr('href', "/" + href)
+    }
+    /* This function changes the title to match the blog post on a blog post page*/
+    if($('.blog-post').length == 1){
+      var title = "EPX Labs, Inc. | " + $('h1').text()
+      $(document).prop('title', title)
+    }
+    clickEvents()
+    setInterval(function(){
+      if ($('li.active').length > 1){
+        if ($(currentMousePos).attr('id') == "contact"){
+          $($('li.active')[0]).removeClass('dropdown active')
+        }
+        else {
+          $($('li.active')[1]).removeClass('dropdown active')
+        }
       }
-      else {
-        $($('li.active')[1]).removeClass('dropdown active')
+      else if ($('li.active').length < 1){
+        $('li#contact-us').addClass('active')
       }
-  }
-  else if ($('li.active').length < 1){
-     $('li#contact-us').addClass('active')
-  }
-  clickEvents()
-  }, 1)})
+      clickEvents()
+    }, 1)})
 
 
 
-var disqus_config = function () {
-this.page.url = "http://www.epxlabs.com";  // Replace PAGE_URL with your page's canonical URL variable
-this.page.identifier = window.location.pathname; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-};
+    var disqus_config = function () {
+      this.page.url = "http://www.epxlabs.com";  // Replace PAGE_URL with your page's canonical URL variable
+      this.page.identifier = window.location.pathname; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
 
-(function() { // DON'T EDIT BELOW THIS LINE
-var d = document, s = d.createElement('script');
-s.src = '//www-epxlabs-com.disqus.com/embed.js';
-s.setAttribute('data-timestamp', +new Date());
-(d.head || d.body).appendChild(s);
-})();
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = '//www-epxlabs-com.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+  })();
